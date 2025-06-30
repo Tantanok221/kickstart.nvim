1bd84c39a3ad17d73756b43bfd59e244f58fc30b
Tantanok221 <6572withspicy@gmail.com>
2025-06-30 12:13:00 +0800

    update config

--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -1,3 +1,4 @@
 vim.keymap.set('n', '<leader>cc', '<cmd>ClaudeCode<CR>', { desc = 'Toggle Claude Code' })
 vim.keymap.set('n', '\\', ':Telescope file_browser<CR>')
 vim.keymap.set('n', '<leader>H', ':Alpha<CR>', { desc = 'Go Back to Dashboard' })
+vim.keymap.set('n', '<leader>Q', ':wqa<CR>', { desc = 'Quit Neovim' })
--- lua/custom/plugins/alphadashboard.lua
+++ lua/custom/plugins/alphadashboard.lua
@@ -5,11 +5,11 @@ return {
     local dashboard = require 'alpha.themes.dashboard'
     dashboard.section.buttons.val = {
       dashboard.button('e', '  > New file', ':ene <BAR> startinsert <CR>'),
-      dashboard.button('f', '  > Find file', ':Telescope find_files<CR>'),
+      dashboard.button('s', '  > Search file', ':Telescope find_files<CR>'),
       dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
       dashboard.button('g', '󰊢  > LazyGit', ':LazyGit <CR>'),
       dashboard.button('<leader>cc', '  > Claude Code', ':ClaudeCode <CR>'),
-      dashboard.button('s', '  > Config', ':Telescope find_files cwd=~/.config/nvim/ <CR>'),
+      dashboard.button('c', '  > Config', ':Telescope find_files cwd=~/.config/nvim/ <CR>'),
       dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
     }
     alpha.setup(dashboard.opts)
