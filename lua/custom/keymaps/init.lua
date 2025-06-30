be169713534f82d392d7910683979f3a729fd60f
Tantanok221 <6572withspicy@gmail.com>
2025-06-30 12:08:13 +0800

    update

--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<leader>cc', '<cmd>ClaudeCode<CR>', { desc = 'Toggle Claude Code' })
 vim.keymap.set('n', '\\', ':Telescope file_browser<CR>')
+vim.keymap.set('n', '<leader>H', ':Alpha<CR>', { desc = 'Go Back to Dashboard' })
--- lua/custom/plugins/alphadashboard.lua
+++ lua/custom/plugins/alphadashboard.lua
@@ -9,7 +9,7 @@ return {
       dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
       dashboard.button('g', '󰊢  > LazyGit', ':LazyGit <CR>'),
       dashboard.button('<leader>cc', '  > Claude Code', ':ClaudeCode <CR>'),
-      dashboard.button('s', '  > Settings', ':Telescope find_files cwd=~/.config/nvim/ <CR>'),
+      dashboard.button('s', '  > Config', ':Telescope find_files cwd=~/.config/nvim/ <CR>'),
       dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
     }
     alpha.setup(dashboard.opts)
